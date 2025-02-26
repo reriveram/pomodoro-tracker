80e3d630924ba401f0daf03cce395ea312a7304b
Rodrigo Rivera <rodriesteban12@gmail.com>
2025-02-26 00:09:43 -0500

    feat: add children inside circular progress

--- src/components/circular-progress/circular-progress.tsx
+++ src/components/circular-progress/circular-progress.tsx
@@ -1,6 +1,12 @@
-import { Dimensions } from "react-native";
+import { Dimensions, Pressable, Text, View } from "react-native";
 import React from "react";
-import Svg, { Circle } from "react-native-svg";
+import Svg, {
+  Circle,
+  G,
+  LinearGradient,
+  RadialGradient,
+  Stop,
+} from "react-native-svg";
 import Animated, {
   useAnimatedProps,
   withSpring,
@@ -11,11 +17,18 @@ import { getStrokeColor } from "@/components/circular-progress/utils";
 interface ICircularProgressProps {
   progress: number;
   cycle: Cycle;
+  children: React.ReactNode;
+  onPress: () => void;
 }
 
 const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
-const CircularProgress = ({ progress, cycle }: ICircularProgressProps) => {
+const CircularProgress = ({
+  progress,
+  cycle,
+  children,
+  onPress,
+}: ICircularProgressProps) => {
   const { width } = Dimensions.get("window");
   const size = width - 32;
   const strokeWidth = 15;
@@ -36,31 +49,39 @@ const CircularProgress = ({ progress, cycle }: ICircularProgressProps) => {
   });
 
   return (
-    <Svg width={size} height={size}>
-      <Circle
-        cx={size / 2}
-        cy={size / 2}
-        r={radius}
-        fill="none"
-        stroke="#404040"
-        strokeWidth={strokeWidth}
-        strokeDashoffset={circumference}
-      />
-      <AnimatedCircle
-        stroke={getStrokeColor(cycle)}
-        fill="none"
-        cx={size / 2}
-        cy={size / 2}
-        r={radius}
-        strokeWidth={strokeWidth}
-        strokeLinecap="round"
-        strokeDasharray={`${circumference} ${circumference}`}
-        strokeDashoffset={circumference}
-        animatedProps={animatedProps}
-        rotation="-90"
-        origin={`${size / 2}, ${size / 2}`}
-      />
-    </Svg>
+    <Pressable
+      onPress={onPress}
+      style={{ position: "relative", width: size, height: size }}
+    >
+      <View className="absolute top-0 left-0 right-0 bottom-0 items-center justify-center z-10">
+        {children}
+      </View>
+      <Svg width={size} height={size}>
+        <Circle
+          cx={size / 2}
+          cy={size / 2}
+          r={radius}
+          fill="none"
+          stroke="#404040"
+          strokeWidth={strokeWidth}
+          strokeDashoffset={circumference}
+        />
+        <AnimatedCircle
+          stroke={getStrokeColor(cycle)}
+          fill="none"
+          cx={size / 2}
+          cy={size / 2}
+          r={radius}
+          strokeWidth={strokeWidth}
+          strokeLinecap="round"
+          strokeDasharray={`${circumference} ${circumference}`}
+          strokeDashoffset={circumference}
+          animatedProps={animatedProps}
+          rotation="-90"
+          origin={`${size / 2}, ${size / 2}`}
+        />
+      </Svg>
+    </Pressable>
   );
 };
 
